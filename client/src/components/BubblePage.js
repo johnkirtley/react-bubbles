296e992c8df63cfb8090b0e63da15bc2a4894dd7
johnkirtley <kirtleyj16@gmail.com>
2020-02-28 09:21:23 -0800

    Ability to delete color

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -16,7 +16,7 @@ const BubblePage = () => {
         setColorList(res.data)
       })
       .catch(err => console.log('Error getting colors', err))
-  })
+  }, [])
 
   return (
     <>
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const initialColor = {
   color: "",
@@ -21,10 +21,19 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+
+
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`/api/colors/${color.id}`)
+      .then(res => {
+        console.log(res)
+        // updateColors([...colors])
+      })
+      .catch(err => console.log('Error deleting color', err))
   };
 
   return (
@@ -35,11 +44,11 @@ const ColorList = ({ colors, updateColors }) => {
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
               <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+                e.stopPropagation();
+                deleteColor(color)
+              }
+              }>
+                x
               </span>{" "}
               {color.color}
             </span>
